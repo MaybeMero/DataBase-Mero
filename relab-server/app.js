1a1cfb3cc83b5db7eef282dae6503ceb6733abfb
MaybeMero <mero.goyes.maybe.yuleysy@iisgalvanimi.edu.it>
2020-05-24 16:34:22 +0000

    mappa con il cerchio

--- relab-server/app.js
+++ relab-server/app.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = new express();
-
+const cors = require ('cors');
 //Importo la classe per le chiamate al DB
 const sqlUtils = require('./SqlUtils.js'); 
-
+app.use(new cors ());
 app.get('/', function (req, res) {
    //Per connettermi al DB uso il metodo statico sqlUtils.connect
    //Passo come parametro la funzione sqlUtils.makeSqlRequest che verrà lanciata 
@@ -15,6 +15,11 @@ app.get('/ci_vettore/:foglio', function (req, res) {
     console.log(req.params.foglio);
     //richiamo il metodo che ottiene l'elenco dei vettori energetici
     sqlUtils.connect(req, res, sqlUtils.ciVettRequest);
+ });
+  app.get('/ci_geovettore/:lng/:lat/:r', function (req, res) {
+    console.log(req.params);
+    //richiamo il metodo che ottiene l'elenco dei vettori energetici
+    sqlUtils.connect(req, res, sqlUtils.ciVettGeoRequest);
  });
 
 app.listen(3000, function () {
