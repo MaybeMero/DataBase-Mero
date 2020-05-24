e7210051ea0fcab5b83f6329693d25f7787b8930
MaybeMero <mero.goyes.maybe.yuleysy@iisgalvanimi.edu.it>
2020-05-24 18:27:11 +0000

    mappa con cerchio tutto commentato

--- RelabClient/src/app/models/icon.model.ts
+++ RelabClient/src/app/models/icon.model.ts
@@ -1,15 +1,15 @@
-export class Icon { //Commenta qui
+export class Icon { //creo  la clase icon
     public scaledSize:ScaledSize;
     constructor(public url: string, size: number){
         this.scaledSize = new ScaledSize(size,size);
     }
 
-    setSize(size: number) { //Commenta qui
+    setSize(size: number) { //Serve a impostare la dimensione 
         this.scaledSize = new ScaledSize(size,size);
     }
 }
 
-export class ScaledSize { //Commenta qui
+export class ScaledSize { //creo la classe scaledsize
     constructor(
     public width:  number,
     public height: number){}
